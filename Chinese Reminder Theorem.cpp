be03e76affe148b14896a124ac2540c380ad469f
Md Turin <matkhan01@yahoo.com>
2020-05-18 23:13:34 +0600

    Update Chinese Reminder Theorem.cpp

--- Chinese Reminder Theorem.cpp
+++ Chinese Reminder Theorem.cpp
@@ -33,6 +33,40 @@ ll inv(ll a, ll m){
     return (x+m)%m;
 }
 
+/**
+long long t, a[mx], n[mx], ans, lc;
+inline long long normalize(long long x, long long mod) { x %= mod; if (x < 0) x += mod; return x; }
+
+struct GCD_type { long long x, y, d; };
+GCD_type ex_GCD(long long a, long long b){
+    if (b == 0) return {1, 0, a};
+    GCD_type pom = ex_GCD(b, a % b);
+    return {pom.y, pom.x - a / b * pom.y, pom.d};
+}
+
+void CRT(){
+    ans = a[1]; lc = n[1];
+    for(int i = 2; i <= t; i++){
+        auto pom = ex_GCD(lc, n[i]);
+        int x1 = pom.x;
+        int d = pom.d;
+        if((a[i] - ans) % d != 0) {
+            write("Infinity Stones are just a myth\n");
+            exit(0);
+        }
+        ans = normalize(ans + x1 * (a[i] - ans) / d % (n[i] / d) * lc, lc * n[i] / d);
+        lc = lcm(lc, n[i]); // you can save time by replacing above lcm * n[i] /d by lcm = lcm * n[i] / d
+    }
+    write(ans, nl);
+}
+
+void MAIN(int &cs){
+//    write("Case ", cs, ": ");
+    read(t); in(a+1,t); in(n+1,t);
+    CRT();
+}
+*/
+
 ll CRT(){
     ll prod = 1LL, x = 0, pp;
     for(ll i=0; i<n; i++) prod *= num[i];
