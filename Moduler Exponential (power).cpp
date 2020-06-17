71c1b09980eb6e35a197c525f209b14331827946
Md Turin <matkhan01@yahoo.com>
2020-06-17 17:51:14 +0600

    Update Moduler Exponential (power).cpp

--- Moduler Exponential (power).cpp
+++ Moduler Exponential (power).cpp
@@ -2,16 +2,14 @@
 using namespace std;
 
 template <typename T>
-T power(T val, T p, T mod) {
-    val %= mod;
-    T result = 1;
+T power(T v, T p, T m) {
+    v %= m; T r = 1;
     while (p > 0) {
         if (p & 0x1)
-            result = (result*val)%mod;
-        val = (val*val)%mod;
-        p >>= 1;
+            r = (r*1LL*v)%m;
+        v = (v*v)%m; p >>= 1;
     }
-    return result;
+    return r;
 }
 
 int main(int argc, const char** argv) {
