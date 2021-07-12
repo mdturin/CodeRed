132015975f2b1895ba1b5bcf40fc28927baad066
Md Turin <matkhan01@yahoo.com>
2021-07-13 00:57:23 +0600

    updated binary index tree

--- Data Structure/Bit Range Query Point Update.cpp
+++ Data Structure/Bit Range Query Point Update.cpp
@@ -1,51 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-/**
-Source : WiiliamFiset
-Binery Index Tree
-Range Query
-Point Upate
-*/
-
-template<typename T> class bit{
+template<typename T> class Bit{
 private:
     size_t n; vector<T> b;
-    inline int lsb(int x){return (x & -x);}
+
+    inline int lsb(int x){
+        return (x & -x);}
+
     T sum(int i){
         T res = 0;
-        for(; i; res+=b[i], i-=lsb(i));
-        return res;
+        while(i > 0){
+            res += b[i];
+            i -= lsb(i);
+        }return res;
     }
+
 public:
-    bit(size_t _n):n(_n){b.resize(n,0);}
-    // 'V' MUST BE ONE BASE & v[0] should be 0;
-    bit(vector<T> &v):n(v.size()),b(v){
-        for(int i=1; i<n; i++){
-            int p = i + lsb(i);
-            if(p<n) b[p] += b[i];
-        }
-    }
-    T sum(int l, int r){return sum(r)-sum(l-1);}
+    Bit(size_t _n):n(_n+1){
+        b.resize(n+5, 0);}
+
+    T sum(int l, int r){
+        return sum(r+1) - sum(l);}
+
     void add(int i, T v){
-        for(; i<n; i+=lsb(i))
+        for(++i; i<=n; i+=lsb(i))
             b[i] += v;
     }
 };
 
-int main(){
-    
-    int n; cin >> n;
-    vector<int> v(n+1);
-    for(int i=1; i<=n; i++)
-        cin >> v[i];
-
-    bit<int> b(v);
-    cout << b.sum(1, 5) << '\n';
-    cout << b.sum(5, 5) << '\n';
-
-    b.add(4, 4);
-    cout << b.sum(3, 5) << '\n';
-
-    return 0;
-}
+int main(){return 0;}
--- Data Structure/Bit Range Query Range Update.cpp
+++ Data Structure/Bit Range Query Range Update.cpp
@@ -1,57 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-/**
-Source : Cp-Algo
-Binery Index Tree
-Range Query
-Range Upate
-*/
-
-/// 1 base indexing please
-template<typename T> class bit{
+template<typename T> class Bit{
 private:
-    size_t n;
-    vector<T> b[2];
-    int lsb(int x){return (x & -x);}
+    size_t n; vector<T> b[2];
+
+    inline int lsb(int x){
+        return (x & -x);}
+
     void add(int p, int i, T x){
-        while(i<n){
+        while(i<=n){
             b[p][i] += x;
             i += lsb(i);
         }
     }
+
     T sum(int p, int i){
         T res = 0;
         while(i>0){
             res += b[p][i];
-            i &= ~lsb(i);
-        }
-        return res;
+            i -= lsb(i);
+        }return res;
     }
-    T sum(int i){return sum(0,i)*i - sum(1,i);}
+
+    T sum(int i){
+        return 1LL*sum(0,i)*i - sum(1,i);}
 
 public:
-    bit(size_t _n):n(_n){
-        b[0].resize(n), b[1].resize(n);}
-    bit(vector<T> &v):bit(v.size()){
-        for(int i=1; i<n; i++)
-            update(i, i, v[i]);
+    Bit(size_t _n) : n(_n+1){
+        b[0].resize(n+5);
+        b[1].resize(n+5);
     }
+
     void update(int l, int r, T x){
+        ++l; ++r;
         add(0, l, x);
         add(0, r+1, -x);
         add(1, l, x*(l-1));
         add(1, r+1, -x*r);
     }
-    T query(int l, int r){return sum(r)-sum(l-1);}
+
+    T query(int l, int r){
+        return sum(r+1) - sum(l);}
 };
 
-int main(){
-    vector<int> v = {0, 1, 2, 3, 4, 5};
-    bit<int> b(v);
-    cout << b.query(1, 5) << '\n';
-    b.update(1, 3, 2);
-    b.update(2, 4, -3);
-    cout << b.query(1, 5) << '\n';
-    return 0;
-}
+int main(){return 0;}
