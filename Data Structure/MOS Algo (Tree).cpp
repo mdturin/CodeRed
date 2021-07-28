c5505cfa6eda28b478fa99847954c0a49b10db25
Md Turin <matkhan01@yahoo.com>
2021-07-28 12:54:02 +0600

    mos tree added & ST updated

---
+++ Data Structure/MOS Algo (Tree).cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int mx = 2e5+5;
+const int lg = __lg(mx);
+
+vector<int> g[mx]; int vis[mx];
+int n, q, cur, a[mx], lvl[mx], par[mx][lg];
+int blk[mx], id[mx], L[mx], R[mx], cnt[mx], ans[mx];
+
+struct query{
+    int l, r, lc, id;
+    bool operator < (const query &ot){
+        if(blk[l] != blk[ot.l])
+            return blk[l] < blk[ot.l];
+        return r < ot.r;
+    }
+}qq[mx];
+
+void dfs(int u, int p, int h){
+    L[u] = ++cur; id[cur] = u;
+    lvl[u] = h; par[u][0] = p;
+    for(int i=1; i<lg; i++)
+        par[u][i] = par[par[u][i-1]][i-1];
+    for(int v : g[u]) if(v != p)
+        dfs(v, u, h+1);
+    R[u] = ++cur; id[cur] = u;
+}
+
+inline int lca(int u, int v){
+    if(lvl[u] > lvl[v]) swap(u, v);
+    for(int i=lg-1; i>=0; i--)
+        if(lvl[v] - (1<<i) >= lvl[u])
+            v = par[v][i];
+    if(u == v) return u;
+    for(int i=lg-1; i>=0; i--)
+        if(par[u][i] != par[v][i])
+            u = par[u][i], v = par[v][i];
+    return par[u][0];
+}
+
+int res = 0;
+inline void check(int x){
+    if(vis[x]){
+        vis[x] = 0; --cnt[a[x]];
+        if(cnt[a[x]] == 0) res--;
+    }else{
+        vis[x] = 1; ++cnt[a[x]];
+        if(cnt[a[x]] == 1) res++;
+    }
+}
+
+void compute(){
+    int curl = qq[0].l, curr = qq[0].l-1;
+    for(int i=0; i<q; i++){
+        while(curl < qq[i].l) check(id[curl++]);
+        while(curl > qq[i].l) check(id[--curl]);
+        while(curr < qq[i].r) check(id[++curr]);
+        while(curr > qq[i].r) check(id[curr--]);
+
+        int u = id[curl], v = id[curr];
+        if(qq[i].lc != u && qq[i].lc != v)
+            check(qq[i].lc);
+        ans[qq[i].id] = res;
+        if(qq[i].lc != u && qq[i].lc != v)
+            check(qq[i].lc);
+    }
+}
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> q; cur = 0;
+    for(int i=1; i<=n; i++) cin >> a[i];
+
+    vector<int> tmp(a+1, a+n+1); sort(tmp.begin(), tmp.end());
+    tmp.erase(unique(tmp.begin(), tmp.end()), tmp.end());
+    for(int i=1; i<=n; i++)
+        a[i] = lower_bound(tmp.begin(), tmp.end(), a[i]) - tmp.begin();
+
+    for(int i=1,u,v; i<n; i++){
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }dfs(1, 0, 0);
+
+    int BLK = sqrt(cur);
+    for(int i=1; i<=cur; i++)
+        blk[i] = (i-1) / BLK + 1;
+
+    for(int i=0,u,v; i<q; i++){
+        cin >> u >> v;
+        qq[i].lc = lca(u, v);
+        if(L[u] > L[v]) swap(u, v);
+        if(qq[i].lc == u)
+            qq[i].l = L[u], qq[i].r = L[v];
+        else qq[i].l = R[u], qq[i].r = L[v];
+        qq[i].id = i;
+    }sort(qq, qq + q);
+
+    compute();
+
+    for(int i=0; i<q; i++)
+        cout << ans[i] << "\n";
+
+    return 0;
+}
--- Data Structure/Segment Tree.cpp
+++ Data Structure/Segment Tree.cpp
@@ -2,53 +2,96 @@
 using namespace std;
 
 const int mod = 1e9 + 7;
-const int mx  = 2e5+5;
+const int mx  = 2e5 + 5;
 
-#define left(p)  (p<<1)
-#define right(p) (p<<1)+1
-#define mid(l,r) (l + (r-l)/2)
+// segment tree (1 - index)
+struct STree{
+private:
+    // change according to problem
+    struct node{
+        int x;
+        node(int _x=0):x(_x){}
+    };
 
-int n, q, a[mx], tree[mx<<2];
+    int n; vector<int> a; vector<node> t;
 
-void build(int p, int l, int r){
-    if(l == r) tree[p] = a[l];
-    else{
-        build(left(p), l, mid(l,r));
-        build(right(p), mid(l,r)+1, r);
-        tree[p] = tree[left(p)] + tree[right(p)];
+    // merge of left and right node change according to problem
+    node merge(node l, node r){
+        node ans;
+        ans.x = l.x + r.x;
+        return ans;
     }
-}
 
-void update(int p, int l, int r, int &i, int &j, int v){
-    if(l>r || l>j || r<i) return;
-    if(l>=i && r<=j){
-        tree[p] += (r-l+1)*v;
-        return;
+    void _b(int p, int l, int r){
+        if(l == r) t[p] = node(a[l]);
+        else{
+            int m = (l + r)/2, cl = p<<1, cr = cl|1;
+            _b(cl, l, m); _b(cr, m+1, r);
+            t[p] = merge(t[cl], t[cr]);
+        }
     }
-    update(left(p), l, mid(l,r), i, j, v);
-    update(right(p), mid(l,r)+1, r, i, j, v);
-    tree[p] = tree[left(p)] + tree[right(p)];
-}
 
-int query(int p, int l, int r, int &i, int &j){
-    if(l>r || l>j || r<i) return 0;
-    if(l>=i && r<=j) return tree[p];
-    int u = query(left(p), l, mid(l,r), i, j);
-    int v = query(right(p), mid(l,r)+1, r, i, j);
-    return u + v;
-}
+    void _u(int p, int l, int r, int idx){
+        if(l == r){t[p] = node(a[l]); return;}
+        int m=(l+r)/2, cl=p<<1, cr=cl|1;
+        if(idx <= m) _u(cl, l, m, idx);
+        else _u(cr, m+1, r, idx);
+        t[p] = merge(t[cl], t[cr]);
+    }
+
+    node _q(int p, int l, int r, int i, int j){
+        if(l>r || l>j || r<i) return node();
+        if(l>=i && r<=j) return t[p];
+        int m=(l+r)/2, cl=p<<1, cr=cl|1;
+        return merge(_q(cl, l, m, i, j),
+                     _q(cr, m+1, r, i, j));
+    }
+
+public:
+    STree(int _n):n(_n){t.resize(n << 2);}
+
+    void build(int _a[]){
+        a = vector<int>(_a, _a+n+1);
+        _b(1, 1, n);
+    }
+
+    void build(vector<int> &_a){
+        a = _a; _b(1, 1, n);
+    }
+
+    void update(int idx, int val){
+        a[idx] = val;
+        _u(1, 1, n, idx);
+    }
+
+    int query(int i, int j){
+        return _q(1, 1, n, i, j).x;
+    }
+};
 
 int main(){
-    cin >> n;
-    for(int i=0; i<n; i++)
-        cin >> a[i];
-        
-    build(1, 0, n-1);
 
-    int u, v; cin >> q;
-    while(q--){
-        cin >> u >> v; u--; v--;
-        cout << query(1, 0, n-1, u, v) << "\n";
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a[15], n = 10;
+    for(int i=1; i<=n; i++)
+        a[i] = i;
+
+    STree t(n); t.build(a);
+    for(int i=1; i<=n; i++){
+        for(int j=i; j<=n; j++){
+            cout << i << " " << j << " - ";
+            cout << t.query(i, j) << "\n";
+        }
+    }
+
+    t.update(5, -5);
+    for(int i=1; i<=n; i++){
+        for(int j=i; j<=n; j++){
+            cout << i << " " << j << " - ";
+            cout << t.query(i, j) << "\n";
+        }
     }
 
     return 0;
