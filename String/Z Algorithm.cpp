ffe482d02c4e596049e58f7bf236c8a95fc245a8
Md Turin <matkhan01@yahoo.com>
2021-07-29 19:47:18 +0600

    z algo updated

--- String/Z Algorithm.cpp
+++ String/Z Algorithm.cpp
@@ -1,60 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef pair<int,int> pii;
-
-const double eps=3e-8;
-const int mx = 1e5+5;
+/** 
+Z - Algorithm : z[i] is the length of the 
+longest common prefix between s and s[i...n] 
+*/
 
 vector<int> zAlgo(string &s){
-    int n = (int)s.size();
-    vector< int > z(n);
+    int n = s.size(); vector<int> z(n);
     for(int i=1, l=0, r=0; i<n; i++){
         if(i <= r) z[i] = min(r-i+1, z[i-l]);
         while(i+z[i]<n && s[z[i]] == s[i+z[i]])
             z[i]++;
         if(i+z[i]-1 > r) l = i, r = i + z[i] - 1;
-    }
-    return move(z);
+    }return move(z);
 }
 
 int stringMatching(string &pat, string &text){
     string modifiedTest = pat + '$' + text;
-    auto ans = zAlgo(modifiedTest);
+    vector<int> ans = zAlgo(modifiedTest);
     int res = 0;
-    for(int u:ans)
+    for(int u : ans)
         if(u == pat.size())
             res++;
     return res;
 }
 
 int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     string pat = "hello";
     string text = "helloWorldhello!!";
-    printf("%d\n", stringMatching(pat,text));
+    cout << stringMatching(pat, text) << "\n";
 
-//    string modifyText = pat + "$" + text;
-//
-//    /** "$" is a unique character
-//    to separate pattarn and text */
-//
-//    auto ans = zAlgo(modifyText);
-    
-//    /** For a string str[0..n-1], Z array is of same length as string. 
-//    An element Z[i] of Z array stores length of the longest substring 
-//    starting from str[i] which is also a prefix of str[0..n-1]. */
-//
-//    /** zAlgo function return the 'Z Array'
-//    where for each index i, it store the longest
-//    length that match with pattarn. so when any
-//    index value is equal to the pattern size it match.
-//    */
-//
-//    int res = 0;
-//    for(int u:ans)
-//        if(u == pat.size())
-//            res++;
-//    PF("%d\n", res); /// res is the total match
     return 0;
 }
