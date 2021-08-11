35842f9418099b5ac6eca3349c62b70ac230d8da
Md Turin <matkhan01@yahoo.com>
2021-08-12 02:11:12 +0600

    mat expo upd

--- Miscellaneous/Matrix Exponential.cpp
+++ Miscellaneous/Matrix Exponential.cpp
@@ -1,35 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-const int mod = 1e9+7;
-const int MS = 2;
-
-#define mem(a) memset(a, 0, sizeof a)
+const int mod = 1e9 + 7;
 
 struct matrix{
-    int mat[MS][MS];
-    matrix(){mem(mat);}
-};/// {{1,1},{1,0}} for fib
+    vector<vector<int>> mat; int MS;
+    matrix(int k){
+        MS = k;
+        mat.resize(k, vector<int>(k, 0));
+    }
+    void print(){
+        for(int i=0; i<MS; ++i)
+        for(int j=0; j<MS; ++j)
+            cout << mat[i][j] << " \n"[j+1==MS];
+    }
+};
 
-matrix init(){
-    matrix res;
-    for(int i=0; i<MS; i++)
+matrix init(int k){
+    matrix res(k);
+    for(int i=0; i<k; i++)
         res.mat[i][i] = 1;
     return move(res);
 }
 
-matrix matrixMultiply(matrix &a, matrix &b){
-    matrix res;
-    for(int i=0; i<MS; i++)
+matrix matrixMultiply(const matrix &a, const matrix &b){
+    int MS = a.MS; 
+    matrix res(MS);
+    for(int i=0; i<MS; i++) 
     for(int j=0; j<MS; j++)
     for(int k=0; k<MS; k++){
-        res.mat[i][j] += (a.mat[i][k] * 1LL * b.mat[k][j])%mod;
+        res.mat[i][j] += a.mat[i][k] * 1LL * b.mat[k][j] % mod;
         if(res.mat[i][j] >= mod) res.mat[i][j] -= mod;
     }return move(res);
 }
 
 long long matrixMultiplyArray(const matrix &m, int *a, int r){
-    long long res = 0;
+    long long res = 0; int MS = m.MS;
     for(int i=0; i<MS; i++){
         res += m.mat[r][i] * 1LL * a[i] % mod;
         if(res >= mod) res -= mod;
@@ -37,7 +42,7 @@ long long matrixMultiplyArray(const matrix &m, int *a, int r){
 }
 
 matrix matrixPower(matrix m, long long p){
-    matrix res = init();
+    matrix res = init(m.MS);
     while(p > 0){
         if(p & 1)
             res = matrixMultiply(res, m);
@@ -49,8 +54,8 @@ void add_val(matrix &m, int i, int j, int v){
     m.mat[i][j] = v;
 }
 
-matrix init_root(){
-    matrix root;
+matrix init_root(int k){
+    matrix root(k);
     add_val(root, 0, 0, 1);
     add_val(root, 0, 1, 1);
     add_val(root, 1, 0, 1);
@@ -62,7 +67,7 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    matrix root = init_root();
+    matrix root = init_root(2);
     long long n; while(cin >> n){
         if(n < 2) cout << n << '\n';
         else{
