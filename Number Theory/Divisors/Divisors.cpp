ae73f3aa78941aec91249ea4c46f288cb2fcac7d
Md Turin <matkhan01@yahoo.com>
2021-08-18 02:55:34 +0600

    upd divisors

--- Number Theory/Divisors/Divisors.cpp
+++ Number Theory/Divisors/Divisors.cpp
@@ -5,27 +5,25 @@ using ll = long long;
 const int mx = 1e6 + 5;
 const int md = 1e9 + 7;
 
-vector<ll> get_divisors(ll x){
-    vector<ll> ans;
-    int lim = sqrtl(x);
-    for(ll i=1; i<=lim; i++){
-        if(x%i == 0){
-            ans.push_back(i);
-            if(i != x/i)
-                ans.push_back(x/i);
-        }
-    }
-    sort(ans.begin(), ans.end());
+template<typename T = int>
+vector<T> get_divisors(T x){
+    vector<T> ans;
+    T lim = sqrtl(x);
+    for(T i=1; i<=lim; i++){
+        if(x % i) continue;
+        ans.push_back(i);
+        if(i != x/i)
+            ans.push_back(x/i);
+    }sort(ans.begin(), ans.end());
     return move(ans);
 }
-
 
 int main(int argc, const char** argv) {
 
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    vector<ll> d = get_divisors(1e6);
+    vector<int> d = get_divisors<int>(1e6);
     for(int u : d)
         cout << u << " ";
     cout << endl;
