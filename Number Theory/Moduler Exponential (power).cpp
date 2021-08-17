a2657fd583983f542a44b42763da2d42a2177de1
Md Turin <matkhan01@yahoo.com>
2021-08-17 13:33:44 +0600

    power updated

--- Number Theory/Moduler Exponential (power).cpp
+++ Number Theory/Moduler Exponential (power).cpp
@@ -1,17 +1,23 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
+const int mx = 1e6 + 5;
+const int md = 1e9 + 7;
+
 template <typename T>
-T power(T v, T p, T m) {
+T power(T v, T p, T m=md) {
     v %= m; T r = 1;
-    while (p > 0) {
-        if (p & 0x1)
-            r = (r*1LL*v)%m;
-        v = (v*1LL*v)%m; p >>= 1;
-    }
-    return r;
+    while (p > 0){
+        if (p & 1)
+            r = (1LL * r * v) % m;
+        v = (1LL * v * v) % m; p >>= 1;
+    }return r;
 }
 
 int main(int argc, const char** argv) {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     return 0;
 }
