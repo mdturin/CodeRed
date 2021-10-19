f70231fe3809c657653c700967d4399b1187ac7c
Md Turin <matkhan01@yahoo.com>
2021-10-20 03:17:14 +0600

    upd st

--- Data Structure/Segment Tree - ST/Segment Tree.cpp
+++ Data Structure/Segment Tree - ST/Segment Tree.cpp
@@ -4,36 +4,37 @@ using namespace std;
 const int mod = 1e9 + 7;
 const int mx  = 2e5 + 5;
 
-template<class T> struct SegTree{
+template <typename T, const T null_val, class F = function<T(const T, const T)>>
+struct SegTree{
+private:
+    #define L (p<<1)
+    #define R (p<<1|1)
 
-    // Set null_val and merge function
-
-    #define left   ((p)<<1)
-    #define right (((p)<<1)|1)
-
-    vector<T> t;
-    static const T null_val = -1e9;         // change this
-    T merge(T u, T v){return max(u, v);}    // change this
-
-    SegTree(int sz=0){t.resize(sz<<2, 0);}
+    int N; F func; vector<T> t;
 
     void upd(int p, int l, int r, int i, T x){
-        if (l > i || r < i) return;
-        if (l == r && l == i){t[p] = x; return;}
+        if(l>r || l>i || r<i) return;
+        if(l == r){t[p] = x; return;}
         int m = (l + r) >> 1;
-        upd(left, l, m, i, x);
-        upd(right, m+1, r, i, x);
-        t[p] = merge(t[left], t[right]);
+        upd(L, l, m, i, x);
+        upd(R, m+1, r, i, x);
+        t[p] = func(t[L], t[R]);
     }
 
-    T query(int p, int l, int r, int i, int j){
+    T qry(int p, int l, int r, int i, int j){
         if (l>r || l>j || r<i) return null_val;
         if (l>=i && r<=j) return t[p];
         int m = (l + r) >> 1;
-        T u = query(left, l, m, i, j);
-        T v = query(right, m+1, r, i, j);
-        return merge(u, v);
+        T u = qry(L, l, m, i, j);
+        T v = qry(R, m+1, r, i, j);
+        return func(u, v);
     }
+
+public:
+    SegTree(int sz, F f) : N(sz+1), func(f){
+        t.resize(N<<2, null_val);}
+    void update(int i, T x){upd(1, 0, N, i, x);}
+    T query(int i, int j){return qry(1, 0, N, i, j);}
 };
 
 int main(){
@@ -41,7 +42,18 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    SegTree<int> st(10);
+    auto get_sum = [&](int u, int v){
+        return u + v;
+    };
+
+    const int null_val = 0;
+    SegTree<int, null_val> st(10, get_sum);
+
+    for(int i=0; i<10; ++i)
+        st.update(i, i + 10);
+
+    for(int i=3; i<10; ++i)
+        cout << st.query(i-3, i) << endl;
 
     return 0;
 }
