ae50d05aae335e08e57986eb19d4b68903a87eec
Md Turin <matkhan01@yahoo.com>
2021-10-22 20:43:46 +0600

    upd tmplt

--- Reader _ Debug _ Precode/template.cpp
+++ Reader _ Debug _ Precode/template.cpp
@@ -1,9 +1,11 @@
 #include<bits/stdc++.h>
-#include<ext/pb_ds/tree_policy.hpp>
-#include<ext/pb_ds/assoc_container.hpp>
-
 using namespace std;
-using namespace __gnu_pbds;
+
+/******* PBDS Headers *********/
+// #include<ext/pb_ds/tree_policy.hpp>
+// #include<ext/pb_ds/assoc_container.hpp>
+// using namespace __gnu_pbds;
+
 using ll  = long long;
 using vl  = vector<ll>;
 using vi  = vector<int>;
@@ -15,16 +17,17 @@ using tpl = tuple<int,int,int>;
 #define UP template<class T, class U>
 #define HP template<class H, class...T>
 
+/******* GCC Optimizations *********/
 #pragma GCC target("avx,avx2")
 #pragma GCC optimization("O3")
 #pragma GCC optimization("unroll-loops")
 
 /******* PBDS *********/
-#define AT find_by_order // return iterator of index
-#define POS order_of_key // return position of value
-#define ERS(st,x) (st).erase((st).AT((st).POS((x)))) // Erase x value
-TP struct pbds_cmp{bool operator()(const T &a, const T &b)const{return a < b;}};
-UP using oset = tree<T, null_type, U, rb_tree_tag, tree_order_statistics_node_update>;
+// #define AT find_by_order // return iterator of index
+// #define POS order_of_key // return position of value
+// #define ERS(st,x) (st).erase((st).AT((st).POS((x)))) // Erase x value
+// TP struct pbds_cmp{bool operator()(const T &a, const T &b)const{return a < b;}};
+// UP using oset = tree<T, null_type, U, rb_tree_tag, tree_order_statistics_node_update>;
 
 #define gp ' '
 #define nl '\n'
@@ -43,24 +46,19 @@ UP using oset = tree<T, null_type, U, rb_tree_tag, tree_order_statistics_node_up
 #define amax(a,b) a=max(a,b)
 #define amin(a,b) a=min(a,b)
 #define FLUSH fflush(stdout)
-#define G(t, a) get<(a)>((t))
 #define all(x) (x).begin(),(x).end()
 #define Clr(x) memset((x),  0, sizeof((x)))
 #define Set(x) memset((x), -1, sizeof((x)))
 #define For(i,a,b)  for(int i=a;  i<b; i++)
 #define Forn(i,b,a) for(int i=b; i>=a; i--)
 
-IL bool read(char&); IL bool read(char*);
-IL void write(char); IL void write(char*);
-TP IL bool read(T&); HP IL bool read(H&,T&...);
-IL void write(const char*); HP IL void write(H,T...); TP IL void write(T);
+IL void write(char*); TP IL void write(T); IL bool read(char&); IL void write(const char*);
+TP IL bool read(T&); IL bool read(char*); IL void write(char); HP IL void write(H,T...); HP IL bool read(H&,T&...);
 
 TP void in(T *a, size_t n){For(i,0,n) read(a[i]);}
 TP void in(vector<T> &a, size_t n){For(i,0,n) read(a[i]);}
-void out(vi  &a, size_t n){For(i,0,n){if(i)pc(gp);write(a[i]);}puts("");}
-void out(int *a, size_t n){For(i,0,n){if(i)pc(gp);write(a[i]);}puts("");}
-void out( ll *a, size_t n){For(i,0,n){if(i)pc(gp);write(a[i]);}puts("");}
-void out( vl &a, size_t n){For(i,0,n){if(i)pc(gp);write(a[i]);}puts("");}
+TP void out(T *a, size_t n){For(i,0,n){if(i)pc(gp);write(a[i]);}puts("");}
+TP void out(vector<T> &a, size_t n){For(i,0,n){if(i)pc(gp);write(a[i]);}puts("");}
 
 const int INF = 0x3f3f3f3f;
 const ll INFL = 0x7f7f7f7f7f7f7f;
@@ -74,8 +72,6 @@ TP IL T power(T v, T p, T m=MOD) {
         v = (v*1LL*v)%m; p >>= 1;
     }return r;
 }
-IL void No(){write("NO\n"); exit(0);}
-IL void Yes(){write("YES\n"); exit(0);}
 TP IL T gcd(T a, T b){return (b?gcd(b,a%b):a);}
 TP IL T lcm(T a, T b){return (a / gcd(a,b) * b);}
 TP IL T inv(T x, int m=MOD){return power<T>(x, m-2, m);}
@@ -88,45 +84,44 @@ TP IL void unsetBit(T &x, int p){x &= (~(1LL<<p));}
 IL bool isPowerOfTwo(ll x){return x && (!(x & (x-1)));}
 IL int numberOfSetBit(ll x){return __builtin_popcountll(x);}
 
+/** Mod - Int **/
 template <const int32_t MOD> struct modint {
+    #define IM inline modint<MOD>
+    #define TPT template<typename T>
+
     int32_t value; modint() = default;
-    template<typename T> modint(T value_) : value(value_%MOD) {}
-    inline modint<MOD> operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
-    inline modint<MOD> operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
-    inline modint<MOD> operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
-    inline modint<MOD> & operator += (modint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
-    inline modint<MOD> & operator -= (modint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
-    inline modint<MOD> & operator *= (modint<MOD> other) { this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
-    inline modint<MOD> operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
+    TPT modint(T value_) : value(value_%MOD) {}
+    IM operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
+    IM operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
+    IM operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
+    IM &operator += (modint<MOD> other) {this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
+    IM &operator -= (modint<MOD> other) {this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
+    IM &operator *= (modint<MOD> other) {this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
+    IM operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
     modint<MOD> pow(uint64_t k) const { modint<MOD> x = *this, y = 1; for (; k; k >>= 1) { if (k & 1) y *= x; x *= x; } return y; }
     modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime
-    inline modint<MOD> operator /  (modint<MOD> other) const { return *this *  other.inv(); }
-    inline modint<MOD> operator /= (modint<MOD> other)       { return *this *= other.inv(); }
-    inline bool operator == (modint<MOD> other) const { return value == other.value; }
-    inline bool operator != (modint<MOD> other) const { return value != other.value; }
-    inline bool operator < (modint<MOD> other)  const { return value < other.value; }
-    inline bool operator > (modint<MOD> other)  const { return value > other.value; }
+    IM operator /  (modint<MOD> other) const { return *this *  other.inv(); }
+    IM operator /= (modint<MOD> other)       { return *this *= other.inv(); }
+    inline bool operator == (modint<MOD> other) const {return value == other.value;}
+    inline bool operator != (modint<MOD> other) const {return value != other.value;}
+    inline bool operator < (modint<MOD> other)  const {return value < other.value;}
+    inline bool operator > (modint<MOD> other)  const {return value > other.value;}
+
+    TPT IM &operator *= (T ot){return *this *= modint<MOD>(ot);}
+    TPT IM &operator /= (T ot){return *this /= modint<MOD>(ot);}
+    TPT IM &operator += (T ot){return *this += modint<MOD>(ot);}
+    TPT IM &operator -= (T ot){return *this -= modint<MOD>(ot);}
+    TPT IM operator + (T ot) const {return *this + modint<MOD>(ot);}
+    TPT IM operator - (T ot) const {return *this - modint<MOD>(ot);}
+    TPT IM operator * (T ot) const {return *this * modint<MOD>(ot);}
+    TPT IM operator / (T ot) const {return *this / modint<MOD>(ot);}
 };
 
-string to_string(string s){return '"' + s + '"';}
-string to_string(bool b){return (b ? "true" : "false");}
-string to_string(const char* s){return to_string((string) s);}
-template <typename A, typename B> string to_string(pair<A, B> p){
-    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";}
-template <typename A> string to_string(A v) {
-    bool first = true;
-    string res = "{";
-    for (const auto &x : v) {
-        if (!first) res += ", ";
-        first = false;
-        res += to_string(x);
-    }res += "}"; return res;
-}
- 
-void debug_out(){cerr << endl;}
-template <typename Head, typename... Tail>
-void debug_out(Head H, Tail... T){
-    cerr << " " << to_string(H); debug_out(T...);}
+/** Debugging Tools **/
+string to_string(string s){return '"' + s + '"';} string to_string(const char* s){return to_string((string) s);}
+string to_string(bool b){return (b ? "true" : "false");} UP string to_string(pair<T, U> p){return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";}
+TP string to_string(T v){bool first = true; string res = "{"; for(const auto &x : v){if(!first) res += ", "; first = false; res += to_string(x); }res += "}"; return res;}
+void debug_out(){cerr << endl;} HP void debug_out(H Ht, T... Tt){cerr << " " << to_string(Ht); debug_out(Tt...);}
 
 // #define LOCAL
 #ifdef LOCAL
@@ -144,7 +139,7 @@ const int sqmx = sqrt(mx) + 1;
 const int lgmx = __lg(mx) + 1;
 
 string s; char buf[mx];
-using mint = modint<MODF>;
+using mint = modint<MOD>;
 
 //vi g[mx]; int vis[mx];
 int n, m, a[mx], b[mx];
