c3816e273d8000f84cfca9fb8fbcc1ed2194020b
Md Turin <matkhan01@yahoo.com>
2021-10-22 20:41:47 +0600

    upd modint

--- Miscellaneous/Mod-Int.cpp
+++ Miscellaneous/Mod-Int.cpp
@@ -6,31 +6,37 @@ using ii = pair<int,int>;
 const int md = 1e9 + 7;
 const int mx = 1e6 + 5;
 
-template <const int MOD>
-struct modint {
-    int value;
-    modint() = default;
-    modint(int value_) : value(value_) {}
-    inline modint<MOD> operator + (modint<MOD> other) const { int c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
-    inline modint<MOD> operator - (modint<MOD> other) const { int c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
-    inline modint<MOD> operator * (modint<MOD> other) const { int c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
-    inline modint<MOD> & operator += (modint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
-    inline modint<MOD> & operator -= (modint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
-    inline modint<MOD> & operator *= (modint<MOD> other) { this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
-    inline modint<MOD> operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
-    modint<MOD> pow(uint64_t k) const { modint<MOD> x = *this, y = 1; for (; k; k >>= 1) { if (k & 1) y *= x; x *= x; } return y; }
-    modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime
-    inline modint<MOD> operator /  (modint<MOD> other) const { return *this *  other.inv(); }
-    inline modint<MOD> operator /= (modint<MOD> other)       { return *this *= other.inv(); }
-    inline bool operator == (modint<MOD> other) const { return value == other.value; }
-    inline bool operator != (modint<MOD> other) const { return value != other.value; }
-    inline bool operator < (modint<MOD> other) const { return value < other.value; }
-    inline bool operator > (modint<MOD> other) const { return value > other.value; }
-};
+template <const int32_t MOD> struct modint { 
+    #define IM inline modint<MOD>
+    #define TPT template<typename T>
 
-template <int32_t MOD> ostream & operator << (ostream & out, modint<MOD> n) { return out << n.value;}
-template <int32_t MOD> modint<MOD> operator * (int32_t value, modint<MOD> n) { return modint<MOD>(value%MOD) * n;}
-template <int32_t MOD> modint<MOD> operator * (int64_t value, modint<MOD> n) { return modint<MOD>(value%MOD) * n;}
+    int32_t value; modint() = default; 
+    TPT modint(T value_) : value(value_%MOD) {} 
+    IM operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); } 
+    IM operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); } 
+    IM operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); } 
+    IM &operator += (modint<MOD> other) {this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; } 
+    IM &operator -= (modint<MOD> other) {this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; } 
+    IM &operator *= (modint<MOD> other) {this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; } 
+    IM operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); } 
+    modint<MOD> pow(uint64_t k) const { modint<MOD> x = *this, y = 1; for (; k; k >>= 1) { if (k & 1) y *= x; x *= x; } return y; } 
+    modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime 
+    IM operator /  (modint<MOD> other) const { return *this *  other.inv(); } 
+    IM operator /= (modint<MOD> other)       { return *this *= other.inv(); } 
+    inline bool operator == (modint<MOD> other) const {return value == other.value;} 
+    inline bool operator != (modint<MOD> other) const {return value != other.value;} 
+    inline bool operator < (modint<MOD> other)  const {return value < other.value;} 
+    inline bool operator > (modint<MOD> other)  const {return value > other.value;}
+
+    TPT IM &operator *= (T ot){return *this *= modint<MOD>(ot);} 
+    TPT IM &operator /= (T ot){return *this /= modint<MOD>(ot);} 
+    TPT IM &operator += (T ot){return *this += modint<MOD>(ot);} 
+    TPT IM &operator -= (T ot){return *this -= modint<MOD>(ot);} 
+    TPT IM operator + (T ot) const {return *this + modint<MOD>(ot);} 
+    TPT IM operator - (T ot) const {return *this - modint<MOD>(ot);} 
+    TPT IM operator * (T ot) const {return *this * modint<MOD>(ot);} 
+    TPT IM operator / (T ot) const {return *this / modint<MOD>(ot);}  
+}; 
 
 using mint = modint<md>;
 
