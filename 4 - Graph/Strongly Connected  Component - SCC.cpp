f9e9e26551dd70ab6fa87d0ed8c4cf2248ba5f03
Md Turin <matkhan01@yahoo.com>
2021-11-05 23:35:37 +0600

    upd scc

--- 4 - Graph/Strongly Connected  Component - SCC.cpp
+++ 4 - Graph/Strongly Connected  Component - SCC.cpp
@@ -5,70 +5,65 @@ using ii = pair<int, int>;
 #define ff first
 #define se second
 
-const int mx = 1e6 + 5;
+const int mx = 1e5 + 5;
 const int inf = 0x3f3f3f3f;
 
-int n, m;
-vector<vector<int>> g, gr;
-
-struct SCC{
+template<int SZ> struct SCC{
+    int N, comp[SZ];
+    bitset<SZ> visit;
     vector<vector<int>> scc;
-    vector<int> col, vis, ts;
-
-    void add(int u, int v){
-        g[u].push_back(v);
-        gr[v].push_back(u);
+    vector<int> ts, allComp;
+    vector<int> g[SZ], gr[SZ];
+    
+    void add(int a, int b){
+        g[a].push_back(b); 
+        gr[b].push_back(a);
     }
-
+    
     void topo_dfs(int u){
-        vis[u] = 1;
+        visit[u] = 1;
         for(int &v : g[u])
-            if(!vis[v]) 
+            if(!visit[v])
                 topo_dfs(v);
         ts.push_back(u);
     }
-
-    void scc_dfs(int u){
-        col[u] = scc.size();
+    
+    void scc_dfs(int u, int val){
+        comp[u] = val;
         for(int &v : gr[u])
-            if(col[v] == 0)
-                scc_dfs(v);
+            if(comp[v] == -1)
+                scc_dfs(v, val);
         scc.back().push_back(u);
     }
-
-    void build(int N){
-        vis.assign(N, 0);
-        col.assign(N, 0);
-
-        ts.clear();
+    
+    void build(int _N){
+        N = _N; scc.clear();
         for(int i=0; i<N; ++i)
-            if(!vis[i]) topo_dfs(i);
+            comp[i] = -1, visit[i] = 0;
+        for(int i=0; i<N; ++i)
+            if(!visit[i]) topo_dfs(i);
         reverse(ts.begin(), ts.end());
-
-        for(int &u : ts) {
-            if(col[u]) continue;  
+        for(int &i : ts) if(comp[i] == -1){
             scc.emplace_back();
-            scc_dfs(u);
+            scc_dfs(i, i);
+            allComp.push_back(i);
         }
     }
 
     vector<vector<int>> get_scc(){return move(scc);}
-    bool is_same(int u, int v){return col[u]==col[v];}
 };
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
 
+    int n, m;
     cin >> n >> m;
-    
-    g.resize(n);
-    gr.resize(n);
 
-    SCC sol;
+    SCC<10000> sol;
     int u, v; while(m--){
-        cin >> u >> v; --u; --v;
-        sol.add(u, v);
+        cin >> u >> v; 
+        --u; --v; sol.add(u, v);
     }
     
     sol.build(n);
