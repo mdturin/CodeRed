9415c5141d1c54efd1d51399c66c7cdcd1c9dc17
Md Turin <matkhan01@yahoo.com>
2021-11-12 23:24:16 +0600

    add simple

---
+++ 8 - Reader _ Debug _ Precode/Template - Simple.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using ii = pair<int, int>;
+
+const int md = 1e9 + 7;
+const int mx = 1e6 + 5;
+
+string s;
+int n, a[mx];
+
+inline void MAIN(){
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+
+    int tc = 1; 
+    // cin >> tc;
+    for(int cs=1; cs<=tc; ++cs){
+        // cout << "Case " << cs << ": ";
+        MAIN();
+    }
+
+    return 0;
+}
