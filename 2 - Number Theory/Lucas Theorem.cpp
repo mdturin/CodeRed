8090d691e95c212ba11637d863a345b90d46564d
Md TUrin <matkhan01@yahoo.com>
2022-09-02 22:12:35 +0600

    fix lucas

--- 2 - Number Theory/Lucas Theorem.cpp
+++ 2 - Number Theory/Lucas Theorem.cpp
@@ -4,74 +4,43 @@ using ll = long long;
 using ii = pair<int,int>;
 
 const int mx = 1e6 + 5;
-const int MOD = 1e9 + 7;
+const int MOD = 1e6 + 3;
 
-template <const int32_t MOD> struct modint{
-    #define IM inline modint<MOD>
-    #define TPT template<typename T>
+/**
+ * @brief 
+ * 
+ * Lucas Theoram can compute
+ * larger binomial coefficient
+ * (NcR)%M when the modular value
+ * M is small. (M <= 1e7)
+ *  
+ * @param P 
+ */
 
-    int32_t value; modint() = default;
-    TPT modint(T value_) : value(value_%MOD) {}
-    IM operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
-    IM operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
-    IM operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
-    IM &operator += (modint<MOD> other) {this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
-    IM &operator -= (modint<MOD> other) {this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
-    IM &operator *= (modint<MOD> other) {this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
-    IM operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
-    modint<MOD> pow(uint64_t k) const { modint<MOD> x = *this, y = 1; for (; k; k >>= 1) { if (k & 1) y *= x; x *= x; } return y; }
-    modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime
-    IM operator /  (modint<MOD> other) const { return *this *  other.inv(); }
-    IM operator /= (modint<MOD> other)       { return *this *= other.inv(); }
-    inline bool operator == (modint<MOD> other) const {return value == other.value;}
-    inline bool operator != (modint<MOD> other) const {return value != other.value;}
-    inline bool operator < (modint<MOD> other)  const {return value < other.value;}
-    inline bool operator > (modint<MOD> other)  const {return value > other.value;}
+vector<ll> Fact;
+vector<ll> Invfact;
 
-    TPT IM &operator += (T ot){return *this += modint<MOD>(ot);}
-    TPT IM &operator -= (T ot){return *this -= modint<MOD>(ot);}
-    TPT IM &operator *= (T ot){return *this *= modint<MOD>(ot);}
-    TPT IM &operator /= (T ot){return *this *= modint<MOD>(ot).inv();}
+void compute(int P = MOD){
+	Fact.clear();
+	Fact.resize(P);
+	Invfact.clear();
+	Invfact.resize(P);
+        
+	Fact[0]=Fact[1]=1;
+	Invfact[0]=Invfact[1]=1;
+	
+	for(int i=2;i<P;i++){
+		Fact[i]=(Fact[i-1]*i)%P;
+		Invfact[i]=((P-P/i)*Invfact[P%i])%P;  
+	}
+}
 
-    TPT IM operator + (T ot) const {return *this + modint<MOD>(ot);}
-    TPT IM operator - (T ot) const {return *this - modint<MOD>(ot);}
-    TPT IM operator * (T ot) const {return *this * modint<MOD>(ot);}
-    TPT IM operator / (T ot) const {return *this / modint<MOD>(ot);}
-};
-
-template <const int32_t _M> struct Combo{
-private:
-    using MI = modint<_M>;
-    vector<MI> f, nf, invs;
-    void __build__(int N){
-        f[0] = nf[0] = invs[1] = 1;
-        for (int i = 2; i < N; i++)
-            invs[i] = invs[_M%i] * (-_M/i);
-        for(int i = 1; i < N; i++){
-            f[i] = f[i - 1] * i;
-            nf[i] = nf[i - 1] * invs[i];
-        }
-    }
-public:
-    Combo() = default;
-    Combo(int N=2):f(N),nf(N),invs(N){__build__(N);}
-    MI ncr(int n, int k){
-        return (n<k || k<0) ? 0 : f[n] * nf[k] * nf[n-k];}
-    inline MI fact(int n) {return f[n];}
-    inline MI finv(int n) {return nf[n];}
-    inline MI  inv(int n) {return invs[n];}
-};
-
-using C = Combo<MOD>;
-using mint = modint<MOD>;
-
-C sol(mx);
-mint lucas(ll n, ll r){
-    if(r > n) return 0;
-    if(n < md) 
-        return sol.ncr(n, r);
-    return lucas(n/md, r/md) * 
-           lucas(n%md, r%md);
+ll Lucas(ll N, ll R, int P=MOD){
+	if(R<0 || R>N) return 0;    // when R is out of bound
+	if(R==0 || R==N) return 1;  // when R is in the edge
+	if(N >= P)                  // try lucas for only larger than MOD
+		return (Lucas(N/P,R/P,P)*Lucas(N%P,R%P,P))%P;
+	return (Fact[N]*(Invfact[N-R]*Invfact[R])%P)%P; // try NcR formula O(1)
 }
 
 int main(){
@@ -79,8 +48,8 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    mint ans = lucas(100000000, 2322);
-    cout << ans << "\n";
+    compute();
+    Lucas(10, 3, MOD);
     
     return 0;
 }
